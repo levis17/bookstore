12f1c012c9def73f998bab518c544a7fbb3a108b
Osayuwamen <aigbogunosayu@gmail.com>
2024-10-26 12:48:24 +0100

    setting up the login page

--- auth/login.php
+++ auth/login.php
@@ -5,35 +5,53 @@
 
     if(isset($_POST['submit'])) {
         
-        if(empty($_POST['username']) OR empty($_POST['email']) OR empty($_POST['password'])) {
+        if(empty($_POST['email']) OR empty($_POST['password'])) {
             echo "<script>alert('one or more inputs are empty');</script>";
         } else {
-            $username = $_POST['username'];
+
             $email = $_POST['email'];
             $password = $_POST['password'];
+
+            $login = $conn->query("SELECT * FROM users WHERE email='$email'");
+            $login->execute();
+
+            $fetch = $login->fetch(PDO::FETCH_ASSOC);
+
+            if($login->rowCount() > 0) {
+                
+                if(password_verify($password, $fetch['mypassword'])) {
+                    echo "LOGGED IN";
+                } else {
+                    echo "<script>alert('password or email are wrong');</script>";
+                }
+            } else {
+                echo "<script>alert('password or email are wrong');</script>";
+            }
         }
 
     }
+
+
 ?>
 
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <form class="form-control mt-5">
+                <form class="form-control mt-5" method="POST" action="login.php">
                     <h4 class="text-center mt-3"> Login </h4>
                    
                     <div class="">
                         <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
                         <div class="">
-                            <input type="email"  class="form-control" id="" value="">
+                            <input type="email"  name="email" class="form-control">
                         </div>
                     </div>
                     <div class="">
                         <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
                         <div class="">
-                            <input type="password" class="form-control" id="inputPassword">
+                            <input type="password" name="password" class="form-control" id="inputPassword">
                         </div>
                     </div>
-                    <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">login</button>
+                    <button class="w-100 btn btn-lg btn-primary mt-4 mb-4" name="submit" type="submit">login</button>
 
                 </form>
             </div>
